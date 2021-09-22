0ea2a481e3170309b10e7e35db51fa62a7b2adf0
Jody Septiawan <jody@192.168.1.9>
2021-09-22 09:44:15 +0700

    Update Inline CSS

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,47 @@
 // create style here
+const style = {
+  form: {
+    margin: '16px 20%',
+  },
+  formGroup: {
+    marginBottom: '16px',
+  },
+  formLabel: {
+    marginBottom: '8px',
+    display: 'inline-block',
+  },
+  formInput: {
+    display: 'block',
+    width: '100%',
+    fontSize: '1rem',
+    color: '#212529',
+    backgroundColor: '#fff',
+    border: '1px solid #ced4da',
+    borderRadius: '.25rem',
+    padding: '5px',
+  },
+};
 
 // create Form component
 function Form() {
   return (
     // code inside div
     <div>
-      {/* form */}
+      <form style={style.form}>
+        <div style={style.formGroup}>
+          <label style={style.formLabel} htmlFor="username">
+            Username
+          </label>
+          <input
+            style={style.formInput}
+            type="text"
+            id="username"
+            placeholder="Username"
+          />
+        </div>
+      </form>
     </div>
-  )
+  );
 }
 
-export default Form
+export default Form;
