5075da39a399a50fd51d40c55cc130a0bb24b840
Jody Septiawan <jody@192.168.1.9>
2021-09-22 10:08:48 +0700

    Update Global Css

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,30 +1,29 @@
 // import css modules
-import cssModules from "./Form.module.css";
+import cssModules from './Form.module.css';
 
 const styles = {
   form: {
-    margin: "16px 20% 0",
+    margin: '16px 20% 0',
   },
   formGroup: {
-    marginBottom: "16px",
+    marginBottom: '16px',
   },
   formLabel: {
-    marginBottom: "8px",
-    display: "inline-block",
+    marginBottom: '8px',
+    display: 'inline-block',
   },
   formInput: {
-    display: "block",
-    width: "100%",
-    padding: ".375rem .75rem",
-    fontSize: "1rem",
+    display: 'block',
+    width: '100%',
+    padding: '.375rem .75rem',
+    fontSize: '1rem',
     lineHeight: 1.5,
-    color: "#212529",
-    backgroundColor: "#fff",
-    border: "1px solid #ced4da",
-    borderRadius: ".25rem",
+    color: '#212529',
+    backgroundColor: '#fff',
+    border: '1px solid #ced4da',
+    borderRadius: '.25rem',
   },
 };
-
 
 function Form() {
   return (
@@ -52,9 +51,19 @@ function Form() {
           />
         </div>
         {/* code here */}
+        <div className="form-group">
+          <label htmlFor="gender" className="form-label">
+            Gender
+          </label>
+          <select id="gender" className="form-select">
+            <option>Choose...</option>
+            <option>Male</option>
+            <option>Female</option>
+          </select>
+        </div>
       </form>
     </div>
-  )
+  );
 }
 
-export default Form
+export default Form;
